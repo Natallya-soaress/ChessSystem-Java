d80f0034d47ea9cb1e481dbc5935c1d45411d479
Nátallya Soares <natallyasoares12@gmail.com>
2021-12-27 16:59:50 -0300

    board and piece

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import chess.chessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		chessMatch chessMatch = new chessMatch();
+		UI.printBoard(chessMatch.getPieces());
+
+	}
+
+}
